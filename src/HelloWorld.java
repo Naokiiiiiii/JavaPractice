196c7ccaeb0d25a6acf73619b0c35f8955180175
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-23 14:48:10 +0900

    feat: object orientation

---
+++ src/HelloWorld.java
@@ -0,0 +1,23 @@
+package src;
+
+import java.time.LocalDate;
+
+import src.model.Dog;
+import src.model.Person;
+
+class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World, nice to meet you!");
+
+    Person tom = new Person("Tom", "Smith", LocalDate.of(1984, 6, 15));
+    Person janet = new Person("Janet", "Jackson", LocalDate.of(1985, 12, 3));
+
+    tom.setSpouse(janet);
+
+    Dog fido = new Dog("Fido", LocalDate.of(2018, 1, 1));
+    fido.bark();
+    tom.setPet(fido);
+
+    System.out.println(tom);
+  }
+}
---
+++ src/model/Cat.java
@@ -0,0 +1,7 @@
+package src.model;
+
+public class Cat extends Pet {
+  public void meow() {
+    System.out.println("Meow meow");
+  }
+}
