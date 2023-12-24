704b406dd1022c8d7e8025415e439b53d47fbb23
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-24 16:09:29 +0900

    objectSuperClass

--- src/section2/ObjectOrientation/model/Cat.java
+++ src/section2/ObjectOrientation/model/Cat.java
@@ -4,4 +4,14 @@ public class Cat extends Pet {
   public void meow() {
     System.out.println("Meow meow");
   }
+
+  @Override
+  public String toString() {
+    return "Cat - name: tony";
+  }
+
+  public static void main(String[] args) {
+    Cat cat1 = new Cat();
+    System.out.println(cat1);
+  }
 }
