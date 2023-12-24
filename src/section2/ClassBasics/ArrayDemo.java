b35d1b842d8e7bc4e45157e0f0e31adc12ad923c
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-24 14:07:48 +0900

    array

---
+++ src/section2/ClassBasics/ArrayDemo.java
@@ -0,0 +1,12 @@
+package src.section2.ClassBasics;
+
+public class ArrayDemo {
+  public static void main(String[] args) {
+    System.out.println(args.length);
+    System.out.println(args[0]);
+
+    int[] nums = { 10, 20, 30, 40, 50 };
+    System.out.println(nums.length);
+    System.out.println(nums[1]);
+  }
+}
