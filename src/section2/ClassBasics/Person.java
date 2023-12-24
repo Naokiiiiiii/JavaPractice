2c6c3107d0e65f9b4bf41194380f424ef57f317c
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-24 13:49:52 +0900

    variables

---
+++ src/section2/ClassBasics/Person.java
@@ -0,0 +1,24 @@
+package src.section2.ClassBasics;
+
+import src.section2.ClassBasics.business.Company;
+
+public class Person {
+  private final double PI = 3.14;
+  private final String MY_BIG_CONSTANT = "Hi, this is my constant message";
+  private String firstName = "Jerry";
+  private int age;
+  private long id;
+  private char middleInitial = 'J';
+  private byte myByte;
+  static private Company company = new Company();
+  static private Company company1 = new Company();
+  private NewsAgency agency;
+
+  public void mytestMethod() {
+    var age = 21;
+    var name = "Jake";
+    String jerry = "Jerry";
+    int years = 67;
+    var mary = "Mary";
+  }
+}
