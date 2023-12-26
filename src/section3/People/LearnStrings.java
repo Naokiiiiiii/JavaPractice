e50a6cef6e18541f42d310877065851fd9ea63d7
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-26 15:36:23 +0900

    feat: learnstring

--- src/section3/People/LearnStrings.java
+++ src/section3/People/LearnStrings.java
@@ -15,5 +15,27 @@ public class LearnStrings {
 
     String myText2 = "ABCGEFG";
     System.out.println(myText2.toLowerCase());
+
+    String mytext3 = "a";
+    System.out.println(mytext3.isBlank());
+
+    String myText4 = "here's my awesome firf text";
+    System.out.println(myText4.replace("firf", "nice"));
+
+    String firstName = "   Jake   ";
+    System.out.format("'%s'", firstName.strip());
+
+    String firstName2 = """
+                  first line here
+                        second line here
+        third line here
+        """;
+    System.out.format("'%s'", firstName2.stripIndent());
+
+    System.out.format("'%s'", split(firstName));
+  }
+
+  public static String split(String text) {
+    return text.replace(" ", "");
   }
 }
