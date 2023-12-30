87bf1a7dc158176f28ac37e817c70658f5ec4a82
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-30 16:32:58 +0900

    number

---
+++ src/section5/NumberStuff.java
@@ -0,0 +1,12 @@
+package src.section5;
+
+public class NumberStuff {
+  public static void main(String[] args) {
+    byte myByte = 127;
+    boolean myFlag = true;
+    short myBigShort = 45;
+    char myChar = '9';
+    char myA = 'A';
+    System.out.println(myByte);
+  }
+}
