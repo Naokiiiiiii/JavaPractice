2081dc3facda8103d2a0187a6748d17d576860c2
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-30 12:00:48 +0900

    regex

--- src/section4/RegexPractice.java
+++ src/section4/RegexPractice.java
@@ -3,5 +3,6 @@ package src.section4;
 public class RegexPractice {
   public static void main(String[] args) {
     System.out.println("aat".matches("[^cCb]at"));
+    System.out.println("321222- 1231".matches("\\d{3}[-,]*\\d{3}[-\\s]+\\d{3,4}"));
   }
 }
