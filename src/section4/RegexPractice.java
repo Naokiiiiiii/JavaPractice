150ea517a0f20094336810a449578f89074b7d5c
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-30 14:58:42 +0900

    capturegroup

--- src/section4/RegexPractice.java
+++ src/section4/RegexPractice.java
@@ -1,8 +1,22 @@
 package src.section4;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexPractice {
   public static void main(String[] args) {
     System.out.println("aat".matches("[^cCb]at"));
-    System.out.println("321222- 1231".matches("\\d{3}[-,]*\\d{3}[-\\s]+\\d{3,4}"));
+
+    String regex = "(\\d{3}[-,]*)(\\d{3}[-\\s]+)(\\d{3,4})";
+    String phoneNumber = "232-333-2365";
+
+    System.out.println("321222- 1231".matches(regex));
+
+    Pattern pat = Pattern.compile(regex);
+    Matcher mat = pat.matcher(phoneNumber);
+    
+    if (mat.matches()) {
+      System.out.println(mat.group(0));
+    }
   }
 }
