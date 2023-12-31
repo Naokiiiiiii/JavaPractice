f051964b6278e33d4165cf4358c2619e5632233a
Naokiiiiiii <d.r-1004@i.softbank.jp>
2023-12-31 18:17:23 +0900

    comment

--- src/section5/NumberStuff.java
+++ src/section5/NumberStuff.java
@@ -2,13 +2,13 @@ package src.section5;
 
 public class NumberStuff {
   public static void main(String[] args) {
-    byte myByte = 127;
-    boolean myFlag = true;
-    short myBigShort = 45;
-    char myChar = '9';
-    char myA = 'A';
-    double myDouble = 3.141592;
-    float myFloat = 3.141592f;
+    // byte myByte = 127;
+    // boolean myFlag = true;
+    // short myBigShort = 45;
+    // char myChar = '9';
+    // char myA = 'A';
+    // double myDouble = 3.141592;
+    // float myFloat = 3.141592f;
 
     float num1 = 2.15f;
     float num2 = 1.10f;
