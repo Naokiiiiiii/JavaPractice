b79052b6bbb202d5656c2f7d4bb74579ebb4436c
Naokiiiiiii <d.r-1004@i.softbank.jp>
2024-01-08 18:01:35 +0900

    refactor: analyst

--- src/section8/Employees/java/Analyst.java
+++ src/section8/Employees/java/Analyst.java
@@ -1,7 +1,5 @@
 package src.section8.Employees.java;
 
-import java.text.NumberFormat;
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -9,34 +7,20 @@ import java.util.regex.Pattern;
 public class Analyst extends Employee {
   private int projectCount = 0;
 
-  private final String peopleRegex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w+)(?:,\\s*\\{(?<details>.*)\\})?";
-  private final Pattern peoplePat = Pattern.compile(peopleRegex);
   private final String analystRegex = "\\w+=(?<projectCount>\\w+)";
   private final Pattern analystPat = Pattern.compile(analystRegex);
-  private final NumberFormat moneyFormat = NumberFormat.getCurrencyInstance();
   
   DateTimeFormatter atFormatter = DateTimeFormatter.ofPattern("M/d/yyyy");
 
   public Analyst(String personText) {
-    Matcher peopleMat = peoplePat.matcher(personText);
-    if (peopleMat.find()) {
-      this.lastName = peopleMat.group("lastName");
-      this.firstName = peopleMat.group("firstName");
-      this.dob = LocalDate.from(atFormatter.parse(peopleMat.group("dob")));
-      analystPat.matcher(peopleMat.group("details"));
-      Matcher progMat = analystPat.matcher(peopleMat.group("details"));
-      if (progMat.find()) {
-        this.projectCount = Integer.parseInt(progMat.group("projectCount"));
-      }
+    super(personText);
+    Matcher progMat = analystPat.matcher(peopleMat.group("details"));
+    if (progMat.find()) {
+      this.projectCount = Integer.parseInt(progMat.group("projectCount"));
     }
   }
-
+  
   public int getSalary() {
     return 2500 + projectCount * 2;
-  }
-
-  @Override
-  public String toString() {
-    return String.format("%s, %s: %s", lastName, firstName, moneyFormat.format(getSalary()));
   }
 }
