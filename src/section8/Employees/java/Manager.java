b555e4ce712afd38d3e95a46def3401e206bc0fb
Naokiiiiiii <d.r-1004@i.softbank.jp>
2024-01-08 17:59:23 +0900

    refactor: manager

--- src/section8/Employees/java/Manager.java
+++ src/section8/Employees/java/Manager.java
@@ -1,8 +1,5 @@
 package src.section8.Employees.java;
 
-import java.text.NumberFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,33 +9,17 @@ public class Manager extends Employee {
 
   private final String mgrRegex = "\\w+=(?<orgSize>\\w+),\\w+=(?<dr>\\w+)";
   private final Pattern mgrPat = Pattern.compile(mgrRegex);
-  private final String peopleRegex = "(?<lastName>\\w+),\\s*(?<firstName>\\w+),\\s*(?<dob>\\d{1,2}/\\d{1,2}/\\d{4}),\\s*(?<role>\\w+)(?:,\\s*\\{(?<details>.*)\\})?";
-  private final Pattern peoplePat = Pattern.compile(peopleRegex);
-  private final NumberFormat moneyFormat = NumberFormat.getCurrencyInstance();
-
-  DateTimeFormatter atFormatter = DateTimeFormatter.ofPattern("M/d/yyyy");
-
+  
   public Manager(String personText) {
-    Matcher peopleMat = peoplePat.matcher(personText);
-    if (peopleMat.find()) {
-      this.lastName = peopleMat.group("lastName");
-      this.firstName = peopleMat.group("firstName");
-      this.dob = LocalDate.from(atFormatter.parse(peopleMat.group("dob")));
-      mgrPat.matcher(peopleMat.group("details"));
-      Matcher mgrMat = mgrPat.matcher(peopleMat.group("details"));
-      if (mgrMat.find()) {
-        this.orgSize = Integer.parseInt(mgrMat.group("orgSize"));
-        this.directReports = Integer.parseInt(mgrMat.group("dr"));
-      }
+    super(personText);
+    Matcher mgrMat = mgrPat.matcher(peopleMat.group("details"));
+    if (mgrMat.find()) {
+      this.orgSize = Integer.parseInt(mgrMat.group("orgSize"));
+      this.directReports = Integer.parseInt(mgrMat.group("dr"));
     }
   }
 
   public int getSalary() {
     return 3500 + orgSize * directReports;
-  }
-
-  @Override
-  public String toString() {
-    return String.format("%s, %s: %s", lastName, firstName, moneyFormat.format(getSalary()));
   }
 }
