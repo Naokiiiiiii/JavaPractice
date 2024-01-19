540fe87ea909163f7d45f7067e0c152e9e916bff
石塚直樹 <d.r-1004@i.softbank.jp>
2024-01-19 15:07:17 +0900

    bigdata

--- src/section10/Employee/java/BigData.java
+++ src/section10/Employee/java/BigData.java
@@ -7,9 +7,16 @@ import java.nio.file.Path;
 public class BigData {
     public static void main(String[] args) {
         try {
-            Files.lines(Path.of("src/section10/Employee/java/Hr5m.csv"))
-                .limit(5)
-                .forEach(System.out::println);
+            long startTime = System.currentTimeMillis();
+            Long result = Files.lines(Path.of("src/section10/Employee/java/Hr5m.csv"))
+                .skip(1)
+                .map(s -> s.split(","))
+                .map(arr -> arr[25])
+                .mapToLong(sal -> Long.parseLong(sal))
+                .sum();
+            long endTime = System.currentTimeMillis();
+            System.out.printf("$%,d.00%n", result);
+            System.out.println(endTime - startTime);
         } catch (IOException e) {
             e.printStackTrace();
         }
