e5410f76f80a0fdc3557d8ecbd4f255c3fe36d8b
石塚直樹 <d.r-1004@i.softbank.jp>
2024-01-23 16:57:46 +0900

    optional

--- src/section11/datastore/OptionalTest.java
+++ src/section11/datastore/OptionalTest.java
@@ -1,8 +1,11 @@
 package src.section11.datastore;
 
+import java.time.Year;
 import java.util.Optional;
 
 public class OptionalTest {
+    record Car(String make, String model, Year year) {}
+    record Person(String firstName, String lastName, Car car) {}
     public static void main(String[] args) {
         Optional<String> optMsg = Optional.of("Hello");
         System.out.println(optMsg);
@@ -10,5 +13,11 @@ public class OptionalTest {
         String msg2 = "catastrophe";
         Optional<String> optMsg2 = Optional.ofNullable(msg2);
         System.out.println(optMsg2.filter(s -> s.length() > 3).orElse("Invalid"));
+
+        Person p1 = new Person("Fred", "Flinstone", new Car("Ford", "Bronco", Year.of(1970)));
+        Person p2 = new Person("Mary", "Johnson", new Car("Ford", "Bronco", Year.of(2020)));
+
+        Optional<Person> optPerson = Optional.of(p1);
+        System.out.println(optPerson.map(Person::firstName).orElse("Unknown"));
     }
 }
