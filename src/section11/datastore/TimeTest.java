f434de3efb09bce83c020adc5ce1eb2a766092eb
石塚直樹 <d.r-1004@i.softbank.jp>
2024-01-26 15:38:00 +0900

    timezone

--- src/section11/datastore/TimeTest.java
+++ src/section11/datastore/TimeTest.java
@@ -2,8 +2,11 @@ package src.section11.datastore;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
 public class TimeTest {
@@ -12,8 +15,8 @@ public class TimeTest {
         System.out.println(now.plus(3, ChronoUnit.YEARS));
 
         LocalDate newYears = LocalDate.of(2020, 1, 1);
-        newYears.datesUntil(LocalDate.now(), Period.ofMonths(1))
-            .forEach(System.out::println);
+        // newYears.datesUntil(LocalDate.now(), Period.ofMonths(1))
+        //     .forEach(System.out::println);
 
         System.out.println(now.equals(newYears));
 
@@ -29,5 +32,9 @@ public class TimeTest {
         System.out.printf("%d years, %d months, %d days%n", diff.getYears(), diff.getMonths(), diff.getDays());
 
         System.out.println(Duration.between(lt1, lt2));
+
+        LocalDateTime ldt1 = LocalDateTime.of(d1, lt1);
+
+        System.out.println(ZonedDateTime.of(ldt1, ZoneId.of("-5")));
     }
 }
